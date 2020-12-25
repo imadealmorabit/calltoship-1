d1630b2ef0cb6a83ca6eefd5a29b8fa33ae91bd4
imadealmorabit <imadealmorabit@gmail.com>
2020-12-26 00:18:10 +0100

    creation de l'entitee property

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Property;
 use phpDocumentor\Reflection\Types\This;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,6 +16,19 @@ class PropertyController extends AbstractController {
      */
     public function index(): Response
     {
+        $property = new Property();
+        $property->setTitle('Mon premier bien')
+            ->setRooms(4)
+            ->setBedrooms(3)
+            ->setDescription('Une petite description')
+            ->setSurface(60)
+            ->setFloor(4)
+            ->setHeat(1)
+            ->setCity('Rabat')
+            ->setAddress('agdal')
+            ->setPostalCode('10000');
+
+
         return $this->render('property/index.html.twig', ['current_menu' => 'properties']);
     }
 }
